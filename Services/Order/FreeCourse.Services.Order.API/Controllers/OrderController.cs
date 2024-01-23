e5af63bf99e3e178d993377be83f01fc44cb15f6
İbrahim Ethem CANSIZ <ethem.cansiz@konfides.com>
2024-01-23 16:58:27 +0300

    Order and FakePayment

--- Services/Order/FreeCourse.Services.Order.API/Controllers/OrderController.cs
+++ Services/Order/FreeCourse.Services.Order.API/Controllers/OrderController.cs
@@ -1,4 +1,8 @@
-﻿using FreeCourse.Shared.ControllerBases;
+﻿using FreeCourse.Services.Order.Application.Commands;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,5 +12,27 @@ namespace FreeCourse.Services.Order.API.Controllers
     [ApiController]
     public class OrderController : CustomControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrderController(IMediator mediator, ISharedIdentityService sharedIdentityService)
+        {
+            _mediator = mediator;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var response = await _mediator.Send(new GetOrderbByUserIdQuery { UserId = _sharedIdentityService.GetUserId});
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrder(CreateOrderCommand createOrderCommand)
+        {
+            var response = await _mediator.Send(createOrderCommand);
+            return CreateActionResultInstance(response);
+        }
     }
 }
--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,19 +1,37 @@
+using FreeCourse.Services.Order.Application.Handlers;
 using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Services;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_order";
+    options.RequireHttpsMetadata = false;
+});
 
 builder.Services.AddDbContext<OrderDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), configure =>
     {
-        configure.MigrationsAssembly("FreeCourse.Services.Order.Insfrastructure");
+        configure.MigrationsAssembly("FreeCourse.Services.Order.Infrastructure");
     });
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CreateOrderCommandHandler).Assembly));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
